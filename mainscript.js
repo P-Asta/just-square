b4c210ddbb265c090c54b902ab485d74fa3082b2
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-02-21 21:20:31 +0900

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -53,17 +53,80 @@ class strtBUTTON{
             this.width,
             this.height
         )
-        delete pointerEvent["click"];
+    }
+}
+
+class youtubeBUTTON{
+    constructor (x , y , width , height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        drow.fillStyle = "red"
+        if (pointer["x"] > this.x){
+            if (pointer["x"] < this.x+200){
+                if (pointer["y"] > this.y){
+                    if (pointer["y"] < this.y+100){
+                        drow.fillStyle = "pink";
+                        if (pointerEvent["on"] == 1){drow.fillStyle = "darkred"}
+                        if (pointerEvent["click"] == 1){
+                            window.open("https://www.youtube.com/c/SCRATCHER523/", '_blank')
+                        }
+                    }
+                }
+            }
+        }
+        drow.fillRect(
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        )
+    }
+}
+
+class discordBUTTON{
+    constructor (x , y , width , height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        drow.fillStyle = "blue"
+        if (pointer["x"] > this.x){
+            if (pointer["x"] < this.x+200){
+                if (pointer["y"] > this.y){
+                    if (pointer["y"] < this.y+100){
+                        drow.fillStyle = "lightblue";
+                        if (pointerEvent["on"] == 1){drow.fillStyle = "darkblue"}
+                        if (pointerEvent["click"] == 1){
+                            window.open("https://discord.com/invite/PjHUKkSJYg", '_blank')
+                        }
+                    }
+                }
+            }
+        }
+        drow.fillRect(
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        )
     }
 }
 
 function frame(){
     drow.clearRect(0 , 0 , client.width , client.height);
-    new strtBUTTON(700,200 , 200 , 100);
+    new strtBUTTON(800,300 , 200 , 100);
+    new youtubeBUTTON(1800,10 , 100 , 100);
+    new discordBUTTON(1800,120 , 100 , 100);
 
     mousepointer();
+    delete pointerEvent["click"];
     requestAnimationFrame(frame);
 }
+
 frame()
 
 client.addEventListener("mousemove" , function (pos) {
